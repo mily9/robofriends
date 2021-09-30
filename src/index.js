887736f2ea97244c77f8ad868390dbdacf63f516
Emilia Ashraghi <emiliaashraghi@gmail.com>
2021-09-30 09:16:35 -0400

    rg

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>,
   <h1>Hola</h1>,
   document.getElementById('root')
 );
