220a3a86e48151afc9dbf63018e7a224cb0ff3fd
Emilia Ashraghi <emiliaashraghi@gmail.com>
2021-10-03 20:06:39 -0400

    g

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const SearchBox = ({ searchChange }) => {
+const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <input 
+      <input
         className='pa3 ba b--green bg-lightest-blue'
-        type='search' 
+        type='search'
         placeholder='search robots'
         onChange={searchChange}
       />
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,44 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
+import Scroll from '../components/Scroll';
 import './App.css';
-
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {  //a piece of data that describes out app
+    this.state = {
       robots: [],
       searchfield: ''
     }
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')  //fetch is a methode of window, is a tool for make request to the server
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users}));  //update with the users
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
   }
 
-  onSearchChage = (event) => {
-    this.setState({ searchfield: event.target.value }) 
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
   }
 
   render() {
     const { robots, searchfield } = this.state;
-    const filteredRobots = this.state.robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (!robots.lenght) {
-      return <h1>Loading</h1>
-    } else {return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChage}/>
-        <Scroll>
-          <CardList robots={ filteredRobots } />
-        </Scroll>
-
-      </div> );
-    }
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
   }
 }
 
